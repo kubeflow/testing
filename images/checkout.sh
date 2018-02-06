c2a9ec612a05946443e9016f2c8bff21828a15d4
Jeremy Lewi <jeremy@lewi.us>
2018-02-05 18:37:47 -0800

    checkout.sh should support checking out extra repositories.
    * We need this for E2E testing because we will need to check out multiple
      repositories. For example, to test our ksonnet configs we want to checkout
      tensorflow/k8s so that we can run the tests defined within it.

--- images/checkout.sh
+++ images/checkout.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 #
 # This script is used to bootstrap our prow jobs.
-# The point of this script is to check out the kubeflow/kubeflow repo
-# at the commit corresponding to the Prow job. We can then
-# invoke the launcher script at that commit to submit and
-# monitor an Argo workflow
+# The point of this script is to check out repositories
+# at the commit corresponding to the Prow job. 
+#
+# In addition to understanding the prow environment variables.
+# the environment variable EXTRA_REPOS can be used to specify
+# extra GitHub repositories to checkout.
+# EXTRA_REPOS should be a ; delimited list of the form
+# {REPO_ORG}/{REPO_NAME}@{SHA}
+
 set -xe
 
 SRC_DIR=$1
@@ -37,6 +42,25 @@ git submodule update
 echo Repo is at `git describe --tags --always --dirty`
 git submodule
 git status
+
+# Check out any extra repos.
+IFS=';' read -ra EXTRA_REPOS <<< "${EXTRA_REPOS}"
+for r in "${REPOS[@]}"; do
+  echo "Repo ${r}" 
+  ORG_NAME="$(cut -d'@' -f1 <<< "$r")"
+  EXTRA_ORG="$(cut -d'/' -f1 <<< "$ORG_NAME")"
+  EXTRA_NAME="$(cut -d'/' -f2<<< "$ORG_NAME")"
+  SHA="$(cut -d'@' -f2 <<< "$r")"  
+  URL=https://github.com/${EXTRA_ORG}/${EXTRA_NAME}.git
+  TARGET=${SRC_DIR}/${EXTRA_ORG}/${EXTRA_NAME}
+  if [ ! -d ${TARGET} ]; then
+  	git clone  ${URL} ${TARGET}
+  	cd ${TARGET}
+  	git checkout ${SHA}
+  else
+  	echo Error ${TARGET} already exists not cloning ${URL}
+  fi
+done	
 
 # Check out the kubeflow/testing repo (unless that's the repo being tested)
 # since it contains common utilities.
