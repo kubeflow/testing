0a3dd08ed3beaf3d3615ac4195dc91c2c3d6b2a4
Jeremy Lewi <jeremy@lewi.us>
2018-02-06 13:23:11 -0800

    checkout.sh should support checking out extra repositories. (#12)
    We need this for E2E testing because we will need to check out multiple
    repositories. For example, to test our ksonnet configs we want to checkout
    tensorflow/k8s so that we can run the tests defined within it.
    
    Move test_util from kubeflow/kubeflow to this repository so we can reuse it.

--- images/checkout.sh
+++ images/checkout.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 #
 # This script is used to bootstrap our prow jobs.
-# The point of this script is to check out the kubeflow/kubeflow repo
-# at the commit corresponding to the Prow job. We can then
-# invoke the launcher script at that commit to submit and
-# monitor an Argo workflow
+# The point of this script is to check out repositories
+# at the commit corresponding to the Prow job. 
+#
+# In addition to understanding the prow environment variables.
+# the environment variable EXTRA_REPOS can be used to specify
+# extra GitHub repositories to checkout.
+# EXTRA_REPOS should be a ; delimited list of the form
+# {REPO_ORG}/{REPO_NAME}@{SHA}
+#
+# For a pull request do
+# {REPO_ORG}/{REPO_NAME}@{SHA}:{PULL_NUMBER}
 set -xe
 
 SRC_DIR=$1
@@ -18,10 +25,16 @@ git clone https://github.com/${REPO_OWNER}/${REPO_NAME}.git ${SRC_DIR}/${REPO_OW
 echo Job Name = ${JOB_NAME}
 
 # See https://github.com/kubernetes/test-infra/tree/master/prow#job-evironment-variables
-cd ${SRC_DIR}/${REPO_OWNER}/${REPO_NAME}
+REPO_DIR=${SRC_DIR}/${REPO_OWNER}/${REPO_NAME}
+cd ${REPO_DIR}
 if [ ! -z ${PULL_NUMBER} ]; then
  git fetch origin  pull/${PULL_NUMBER}/head:pr
- git checkout ${PULL_PULL_SHA}
+ if [ ! -z ${PULL_PULL_SHA} ]; then
+ 	git checkout ${PULL_PULL_SHA}
+ else
+ 	git checkout pr
+ fi
+ 
 else
  if [ ! -z ${PULL_BASE_SHA} ]; then
  	# Its a post submit; checkout the commit to test.
@@ -34,13 +47,47 @@ git submodule init
 git submodule update
 
 # Print out the commit so we can tell from logs what we checked out.
-echo Repo is at `git describe --tags --always --dirty`
+echo ${REPO_DIR} is at `git describe --tags --always --dirty`
 git submodule
 git status
 
+# Check out any extra repos.
+IFS=';' read -ra REPOS <<< "${EXTRA_REPOS}"
+echo REPOS=${REPOS}
+for r in "${REPOS[@]}"; do
+  echo "Processing ${r}" 
+  ORG_NAME="$(cut -d'@' -f1 <<< "$r")"
+  EXTRA_ORG="$(cut -d'/' -f1 <<< "$ORG_NAME")"
+  EXTRA_NAME="$(cut -d'/' -f2<<< "$ORG_NAME")"
+  SHA_AND_PR="$(cut -d'@' -f2 <<< "$r")"  
+  SHA="$(cut -d':' -f1 <<< "$SHA_AND_PR")"  
+  EXTRA_PR="$(cut -d':' -s -f2 <<< "$SHA_AND_PR")"
+  URL=https://github.com/${EXTRA_ORG}/${EXTRA_NAME}.git
+  TARGET=${SRC_DIR}/${EXTRA_ORG}/${EXTRA_NAME}
+  if [ ! -d ${TARGET} ]; then
+  	git clone  ${URL} ${TARGET}
+  	cd ${TARGET}
+
+  	if [ ! -z ${EXTRA_PR} ]; then
+  		git fetch origin  pull/${EXTRA_PR}/head:pr
+  		git checkout pr
+
+	  	if [ "$SHA" -ne "HEAD" ]; then
+	  		git checkout ${SHA}
+		fi  		
+  	else  	
+  		git checkout ${SHA}
+	fi  
+	echo ${TARGET} is at `git describe --tags --always --dirty`
+  else
+  	echo Error ${TARGET} already exists not cloning ${URL}
+  fi
+done	
+
 # Check out the kubeflow/testing repo (unless that's the repo being tested)
 # since it contains common utilities.
-# TODO(jlewi): We might want to eventually pin to a particular version that is known to be good.
-if [ ! -d /src/kubeflow/testing ]; then
+# TODO(jlewi): We should get rid of this and just treat kubeflow/testing as
+# an EXTRA_REPOS.
+if [ ! -d ${SRC_DIR}/kubeflow/testing ]; then
 	git clone https://github.com/kubeflow/testing.git ${SRC_DIR}/kubeflow/testing
 fi	
