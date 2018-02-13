d80d85c6eab457a4e3e5429cb6bf7c8a3da618f3
Jeremy Lewi <jeremy@lewi.us>
2018-02-12 17:59:44 -0800

    Support runnig multiple workflows and using a single entrypoint for prow jobs. (#25)
    Currently each repository needs to build a new Docker container. The sole purpose of this container is to specify the path to the Argo workflow to run.
    
    We can simplify this by just allowing each repository to define a "prow_config.yaml" file at the root of the repository and then having a single binary which will read that config file and use it to figure out which workflows to invoke
    
    This fixes #24
    
    We also add support for running multiple workflows.
    
    This will make it easy to start using prow to build our containers on each commit (#19)

---
+++ images/run_workflows.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# This script is meant to be the entrypoint for a prow job.
+# It checkos out a repo and then looks for prow_config.yaml in that
+# repo and uses that to run one or more workflows.
+set -ex
+
+# Checkout the code.
+/usr/local/bin/checkout.sh /src
+
+# Trigger a workflow
+python -m kubeflow.testing.run_e2e_workflow \
+  --project=mlkube-testing \
+  --zone=us-east1-d \
+  --cluster=kubeflow-testing \
+  --bucket=kubernetes-jenkins \
+  --config_file=/src/${REPO_OWNER}/${REPO_NAME}/prow_config.yaml \
+  --repos_dir=/src
--- py/kubeflow/testing/run_e2e_workflow_test.py
+++ py/kubeflow/testing/run_e2e_workflow_test.py
@@ -4,14 +4,28 @@ import unittest
 import mock
 from kubeflow.testing import run_e2e_workflow
 import tempfile
+import yaml
 
 from google.cloud import storage  # pylint: disable=no-name-in-module
 
+
 class TestRunE2eWorkflow(unittest.TestCase):
+
+  def assertItemsMatch(self, expected, actual):
+    """Check that expected matches actual.
+
+    Args:
+      Expected: List of strings. These can be regex.
+      Actual: Actual items.
+    """
+    self.assertEqual(len(expected), len(actual))
+    for index, e in enumerate(expected):
+      self.assertRegexpMatches(actual[index], e)
+
   @mock.patch("kubeflow.testing.run_e2e_workflow.upload_file_to_gcs")
   @mock.patch("kubeflow.testing.run_e2e_workflow.upload_to_gcs")
   @mock.patch("kubeflow.testing.run_e2e_workflow.util.load_kube_config")
-  @mock.patch("kubeflow.testing.run_e2e_workflow.argo_client.wait_for_workflow")
+  @mock.patch("kubeflow.testing.run_e2e_workflow.argo_client.wait_for_workflows")
   @mock.patch("kubeflow.testing.run_e2e_workflow.util.configure_kubectl")
   @mock.patch("kubeflow.testing.run_e2e_workflow.util.run")
   def testMainPresubmit(self, mock_run, mock_configure, mock_wait, *unused_mocks):  # pylint: disable=no-self-use
@@ -27,42 +41,107 @@ class TestRunE2eWorkflow(unittest.TestCase):
 
     args = ["--project=some-project", "--cluster=some-cluster",
             "--zone=us-east1-d", "--bucket=some-bucket",
-            "--component=workflows", "--env=prow"]
+            "--app_dir=/some/dir",
+            "--component=workflows"]
     run_e2e_workflow.main(args)
 
     mock_configure.assert_called_once_with("some-project", "us-east1-d",
                                            "some-cluster",)
-    self.assertItemsEqual(
-      ["ks", "param", "set", "workflows", "name"],
-      mock_run.call_args_list[0][0][0][:-1])
-    # Workflow name will have some random salt at the end.
-    self.assertRegexpMatches(mock_run.call_args_list[0][0][0][-1],
-                             "kubeflow-presubmit-77-[0-9a-z]{4}")
 
-    self.assertItemsEqual(
-      ["ks", "param", "set", "workflows", "prow_env",
-       "BUILD_NUMBER=1234,JOB_NAME=kubeflow-presubmit,JOB_TYPE=presubmit"
-       ",PULL_NUMBER=77,PULL_PULL_SHA=123abc,REPO_NAME=fake_name"
-       ",REPO_OWNER=fake_org"],
-      mock_run.call_args_list[1][0][0])
+    expected_calls = [
+      ["ks", "env", "add", "kubeflow-presubmit-legacy-77-123abc-1234-.*"],
+      ["ks", "param", "set", "--env=.*", "workflows", "name",
+           "kubeflow-presubmit-legacy-77-[0-9a-z]{4}"],
+      ["ks", "param", "set",
+           "--env=.*",
+           "workflows", "prow_env",
+           "BUILD_NUMBER=1234,JOB_NAME=kubeflow-presubmit,JOB_TYPE=presubmit"
+           ",PULL_NUMBER=77,PULL_PULL_SHA=123abc,REPO_NAME=fake_name"
+           ",REPO_OWNER=fake_org"],
+      ["ks", "param", "set",
+           "--env=.*",
+           "workflows", "namespace",
+           "kubeflow-test-infra"],
+      ["ks", "param", "set",
+           "--env=.*",
+           "workflows", "bucket", "some-bucket"],
+      ["ks", "show", "kubeflow-presubmit.*", "-c", "workflows"],
+      ["ks", "apply", "kubeflow-presubmit.*", "-c", "workflows"],
+    ]
 
-    self.assertItemsEqual(
-      ["ks", "param", "set", "workflows", "namespace",
-       "kubeflow-test-infra"],
-      mock_run.call_args_list[2][0][0])
+    for i, expected in enumerate(expected_calls):
+      self.assertItemsMatch(
+        expected,
+        mock_run.call_args_list[i][0][0])
+      self.assertEquals(
+         "/some/dir",
+         mock_run.call_args_list[i][1]["cwd"])
 
-    self.assertItemsEqual(
-      ["ks", "param", "set", "workflows", "bucket", "some-bucket"],
-      mock_run.call_args_list[3][0][0])
+  @mock.patch("kubeflow.testing.run_e2e_workflow.upload_file_to_gcs")
+  @mock.patch("kubeflow.testing.run_e2e_workflow.upload_to_gcs")
+  @mock.patch("kubeflow.testing.run_e2e_workflow.util.load_kube_config")
+  @mock.patch("kubeflow.testing.run_e2e_workflow.argo_client.wait_for_workflows")
+  @mock.patch("kubeflow.testing.run_e2e_workflow.util.configure_kubectl")
+  @mock.patch("kubeflow.testing.run_e2e_workflow.util.run")
+  def testWithConfig(self, mock_run, mock_configure, mock_wait, *unused_mocks):  # pylint: disable=no-self-use
+    """Test creating a workflow from a config file."""
 
-    self.assertItemsEqual(
-      ["ks", "show", "prow", "-c", "workflows"],
-      mock_run.call_args_list[4][0][0])
+    config = {
+      "workflows": [
+        {"app_dir": "kubeflow/testing/workflows",
+         "component": "workflows",
+         "name": "wf",
+        },]
+    }
+    with tempfile.NamedTemporaryFile(delete=False) as hf:
+      yaml.dump(config, hf)
+      name = hf.name
 
-    self.assertItemsEqual(
-      ["ks", "apply", "prow", "-c", "workflows"],
-      mock_run.call_args_list[5][0][0])
+    os.environ["REPO_OWNER"] = "fake_org"
+    os.environ["REPO_NAME"] = "fake_name"
+    os.environ["PULL_NUMBER"] = "77"
+    os.environ["PULL_PULL_SHA"] = "123abc"
+    os.environ["JOB_NAME"] = "kubeflow-presubmit"
+    os.environ["JOB_TYPE"] = "presubmit"
+    os.environ["BUILD_NUMBER"] = "1234"
 
+    args = ["--project=some-project", "--cluster=some-cluster",
+            "--zone=us-east1-d", "--bucket=some-bucket",
+            "--config_file=" + name,
+            "--repos_dir=/src"]
+    run_e2e_workflow.main(args)
+
+    mock_configure.assert_called_once_with("some-project", "us-east1-d",
+                                           "some-cluster",)
+
+    expected_calls = [
+      ["ks", "env", "add", "kubeflow-presubmit-wf-77-123abc-1234-.*"],
+      ["ks", "param", "set", "--env=.*", "workflows", "name",
+           "kubeflow-presubmit-wf-77-[0-9a-z]{4}"],
+      ["ks", "param", "set",
+           "--env=.*",
+           "workflows", "prow_env",
+           "BUILD_NUMBER=1234,JOB_NAME=kubeflow-presubmit,JOB_TYPE=presubmit"
+           ",PULL_NUMBER=77,PULL_PULL_SHA=123abc,REPO_NAME=fake_name"
+           ",REPO_OWNER=fake_org"],
+      ["ks", "param", "set",
+           "--env=.*",
+           "workflows", "namespace",
+           "kubeflow-test-infra"],
+      ["ks", "param", "set",
+           "--env=.*",
+           "workflows", "bucket", "some-bucket"],
+      ["ks", "show", "kubeflow-presubmit.*", "-c", "workflows"],
+      ["ks", "apply", "kubeflow-presubmit.*", "-c", "workflows"],
+    ]
+
+    for i, expected in enumerate(expected_calls):
+      self.assertItemsMatch(
+        expected,
+        mock_run.call_args_list[i][0][0])
+      self.assertEquals(
+         "/src/kubeflow/testing/workflows",
+         mock_run.call_args_list[i][1]["cwd"])
 
 if __name__ == "__main__":
   unittest.main()
