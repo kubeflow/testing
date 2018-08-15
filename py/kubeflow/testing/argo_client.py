1e95349665016258bfb135b777478862b360afd3
Richard Liu <ricliu@google.com>
2018-08-15 14:49:57 -0700

    making trivial changes
    Revert "making trivial changes"
    
    This reverts commit c196ed38d88972cd3cafb63ef3c88150bd0e5789.
    
    Fix test flake

--- py/kubeflow/testing/argo_client.py
+++ py/kubeflow/testing/argo_client.py
@@ -39,6 +39,19 @@ def log_status(workflow):
 @retry(wait_exponential_multiplier=1000, wait_exponential_max=10000,
        stop_max_delay=20*60*1000,
        retry_on_exception=lambda e: not isinstance(e, util.TimeoutError))
+def get_namespaced_custom_object_with_retries(client, namespace, name):
+  """Call get_namespaced_customer_object API with retries.
+
+  Args:
+    client: K8s api client.
+    namespace: namespace for the workflow.
+    name: name of the workflow.
+  """
+  crd_api = k8s_client.CustomObjectsApi(client)
+  return crd_api.get_namespaced_custom_object(
+    GROUP, VERSION, namespace, PLURAL, name)
+
+
 def wait_for_workflows(client, namespace, names,
                       timeout=datetime.timedelta(minutes=30),
                       polling_interval=datetime.timedelta(seconds=30),
@@ -60,15 +73,12 @@ def wait_for_workflows(client, namespace, names,
   Raises:
     TimeoutError: If timeout waiting for the job to finish.
   """
-  crd_api = k8s_client.CustomObjectsApi(client)
   end_time = datetime.datetime.now() + timeout
   while True:
     all_results = []
 
     for n in names:
-      results = crd_api.get_namespaced_custom_object(
-          GROUP, VERSION, namespace, PLURAL, n)
-
+      results = get_namespaced_custom_object_with_retries(client, namespace, n)
       all_results.append(results)
       if status_callback:
         status_callback(results)
