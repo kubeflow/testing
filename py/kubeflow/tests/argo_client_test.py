6ce73a881af5246d79f4c182416ee0c007830828
Richard Liu <ricliu@google.com>
2018-09-22 00:19:19 -0700

    Try to fix unit tests

--- py/kubeflow/tests/argo_client_test.py
+++ py/kubeflow/tests/argo_client_test.py
@@ -12,13 +12,14 @@ class ArgoClientTest(unittest.TestCase):
     self.test_dir = os.path.join(os.path.dirname(__file__), "test-data")
 
   def test_wait_for_workflow(self):
-    with mock.patch("kubeflow.testing.argo_client.k8s_client.ApiClient") as api_client:
+    with mock.patch("kubeflow.testing.argo_client.k8s_client.ApiClient") as mock_client:
       with open(os.path.join(self.test_dir, "successful_workflow.yaml")) as hf:
         response = yaml.load(hf)
 
-      api_client.call_api.return_value = response
-      result = argo_client.wait_for_workflow("some-namespace", "some-set")
-      self.assertIsNotNone(result)
+        client = mock_client.return_value
+        client.call_api.return_value = response
+        result = argo_client.wait_for_workflow("some-namespace", "some-set")
+        self.assertIsNotNone(result)
 
 if __name__ == "__main__":
   unittest.main()
