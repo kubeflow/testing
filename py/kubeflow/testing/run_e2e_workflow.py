d45cd58c705c025804ba5aad17bf885723826233
Richard Liu <ricliu@google.com>
2018-09-21 21:51:28 -0700

    Remove debug msg

--- py/kubeflow/testing/run_e2e_workflow.py
+++ py/kubeflow/testing/run_e2e_workflow.py
@@ -50,7 +50,6 @@ from kubeflow.testing import util
 import uuid
 import sys
 import yaml
-import time
 
 # The namespace to launch the Argo workflow in.
 def get_namespace(args):
@@ -160,9 +159,6 @@ def run(args, file_handler): # pylint: disable=too-many-statements,too-many-bran
             break
         if dir_modified:
           break
-
-    logging.info("Sleeping...")
-    time.sleep(3660)
 
     # Only consider modified files on presubmit. On postsubmit we run
     # all tests.
