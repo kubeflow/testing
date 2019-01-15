c10dd8328585c0fd437101bfb69b61896fc3bc78
Gabriel Wen <gabrielwen@google.com>
2019-01-15 13:06:29 -0800

    First commit of workflows.sh

---
+++ test-infra/nightly-deployment/workflows.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# This script is meant to be the entrypoint for a prow job.
+# It checkos out a repo and then looks for prow_config.yaml in that
+# repo and uses that to run one or more workflows.
+set -ex
+
+# Check out repos we need.
+/usr/local/bin/checkout.sh /src
+
+# TODO(gabrielwen): Trigger create_kf_instance.py here.
