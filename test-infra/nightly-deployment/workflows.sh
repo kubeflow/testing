bfa1b1c9da85dff8c758cac704db9b44ce061c1f
Gabriel Wen <gabrielwen@google.com>
2019-01-17 15:36:28 -0800

    activate service account auth

--- test-infra/nightly-deployment/workflows.sh
+++ test-infra/nightly-deployment/workflows.sh
@@ -12,19 +12,21 @@ REPO_OWNER=kubeflow
 /usr/local/bin/checkout.sh ${SRC_DIR} ${REPO_OWNER} kubeflow
 /usr/local/bin/checkout.sh ${SRC_DIR} ${REPO_OWNER} testing
 
-# TODO(gabrielwen): Do we need to use key.json?
-# export GOOGLE_APPLICATION_CREDENTIALS=/secret/gcp-credentials/key.json
-
 rm -rf /src/kubeflow/testing/test-infra/kf-v0-4-n00
+
+# Activate service account auth.
+export GOOGLE_APPLICATION_CREDENTIALS=/secret/gcp-credentials/key.json
+gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS}
+gcloud config list
 
 gcloud container clusters get-credentials gabrielwen-playground \
   --zone us-east1-d \
   --project gabrielwen-learning
 
 # Trigger create_kf_instance.
-# python -m kubeflow.testing.create_kf_instance \
-#   --base=kf-v0-4 \
-#   --kubeflow_repo=/src/kubeflow/kubeflow \
-#   --apps_dir=/src/kubeflow/testing/test-infra \
-#   --project=gabrielwen-learning \
-#   --oauth_file=gs://deployment-worker-data/kf-iap-oauth.gabrielwen-learning.yaml
+python -m kubeflow.testing.create_kf_instance \
+  --base=kf-v0-4 \
+  --kubeflow_repo=/src/kubeflow/kubeflow \
+  --apps_dir=/src/kubeflow/testing/test-infra \
+  --project=gabrielwen-learning \
+  --oauth_file=gs://deployment-worker-data/kf-iap-oauth.gabrielwen-learning.yaml
