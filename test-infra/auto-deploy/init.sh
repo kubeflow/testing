ff349f9b70f3ca264ec129ed48a8d7488a381595
Gabriel Wen <gabrielwen@google.com>
2019-01-28 16:24:42 -0800

    temp clone from gabrielwen repo

--- test-infra/auto-deploy/init.sh
+++ test-infra/auto-deploy/init.sh
@@ -7,10 +7,13 @@ REPO_OWNER=$2
 PROJECT=$3
 WORKER_CLUSTER=$4
 
+env
+
 # Check out fresh copy of KF and deployment workflow.
 # TODO(gabrielwen): Need to make a seperate workflow to snapshot repos.
 /usr/local/bin/checkout.sh ${SRC_DIR} ${REPO_OWNER} kubeflow
-/usr/local/bin/checkout.sh ${SRC_DIR} ${REPO_OWNER} testing
+git clone git@github.com:gabrielwen/testing.git --branch snapshot-cron --single-branch
+# /usr/local/bin/checkout.sh ${SRC_DIR} ${REPO_OWNER} testing
 
 PYTHONPATH="${PYTHONPATH}:${SRC_DIR}/${REPO_OWNER}/testing/py"
 export PYTHONPATH
