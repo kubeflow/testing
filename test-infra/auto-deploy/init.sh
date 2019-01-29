c1dcac080e8b04bfc0bcbf64e275de4c021f4b99
Gabriel Wen <gabrielwen@google.com>
2019-01-28 17:05:27 -0800

    fix

--- test-infra/auto-deploy/init.sh
+++ test-infra/auto-deploy/init.sh
@@ -12,7 +12,10 @@ env
 # Check out fresh copy of KF and deployment workflow.
 # TODO(gabrielwen): Need to make a seperate workflow to snapshot repos.
 /usr/local/bin/checkout.sh ${SRC_DIR} ${REPO_OWNER} kubeflow
-git clone git@github.com:gabrielwen/testing.git --branch snapshot-cron --single-branch
+git clone https://github.com/gabrielwen/testing.git \
+  ${SRC_DIR}/${REPO_OWNER}/testing \
+  --branch snapshot-cron \
+  --single-branch
 # /usr/local/bin/checkout.sh ${SRC_DIR} ${REPO_OWNER} testing
 
 PYTHONPATH="${PYTHONPATH}:${SRC_DIR}/${REPO_OWNER}/testing/py"
