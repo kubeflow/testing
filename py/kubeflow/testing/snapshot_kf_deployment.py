b6508849655799a973e396cfe48a735c9cc04b9a
Gabriel Wen <gabrielwen@google.com>
2019-01-28 17:50:51 -0800

    add a few flags to snapshot_kf_deployment

--- py/kubeflow/testing/snapshot_kf_deployment.py
+++ py/kubeflow/testing/snapshot_kf_deployment.py
@@ -5,10 +5,27 @@ TODO(gabrielwen): DO NOT SUBMIT without a detailed description of snapshot_kf_de
 
 import argparse
 import logging
+import requests
 import yaml
 
 def main():
-  pass
+  logging.basicConfig(level=logging.INFO,
+                      format=('%(levelname)s|%(asctime)s'
+                              '|%(pathname)s|%(lineno)d| %(message)s'),
+                      datefmt='%Y-%m-%dT%H:%M:%S',
+                      )
+  logging.getLogger().setLevel(logging.INFO)
+
+  parser = argparse.ArgumentParser()
+
+  parser.add_argument(
+    "--project", default="kubeflow-ci", type=str, help=("The project."))
+
+  parser.add_argument(
+    "--github_token_file",
+    default="/secret/github-token/github_token",
+    type=str, help=("The file containing Github API token."))
+
 
 if __name__ == '__main__':
   main()
