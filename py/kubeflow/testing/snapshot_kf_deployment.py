c8ba9105ebd074fea65c3e14fa628040d4985a4b
Gabriel Wen <gabrielwen@google.com>
2019-01-28 17:37:00 -0800

    init commit to snapshot_kf_deployment

---
+++ py/kubeflow/testing/snapshot_kf_deployment.py
@@ -0,0 +1,14 @@
+"""TODO(gabrielwen): DO NOT SUBMIT without one-line documentation for snapshot_kf_deployment.
+
+TODO(gabrielwen): DO NOT SUBMIT without a detailed description of snapshot_kf_deployment.
+"""
+
+import argparse
+import logging
+import yaml
+
+def main():
+  pass
+
+if __name__ == '__main__':
+  main()
