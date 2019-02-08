0559b08194fff04d555183eaeaf60ec5f58a78ea
Gabriel Wen <gabrielwen@google.com>
2019-02-08 10:48:42 -0800

    Add delete_deployment to create_kf_instance

--- py/kubeflow/testing/create_kf_instance.py
+++ py/kubeflow/testing/create_kf_instance.py
@@ -99,12 +99,11 @@ def main(): # pylint: disable=too-many-locals,too-many-statements
   # Clean up previous deployment. We are not able to run "kfctl delete all"
   # since we are not able to guarantee apps config in repository is up to date.
   util.run(["rm", "-rf", name], cwd=args.apps_dir)
-  # TODO(gabrielwen):
-  # https://github.com/kubeflow/testing/issues/295
-  # 1. Is deployment deletion still needed?
-  # 2. If it is, figure out permission set up for it.
-  # 3. Should use
-  # https://github.com/kubeflow/kubeflow/blob/master/scripts/gke/delete_deployment.sh
+
+  delete_deployment = os.path.join(args.kubeflow_repo, "scripts", "gke",
+                                   "delete_deployment.sh")
+  util.run([delete_deployment, "--project=" + args.project,
+            "--deployment=" + name, "--zone=" + args.zone], cwd=args.apps_dir)
 
   # Create a dummy kubeconfig in cronjob worker.
   util.run(["gcloud", "container", "clusters", "get-credentials", args.deployment_worker_cluster,
