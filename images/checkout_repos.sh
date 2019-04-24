e7ec93e510af8e9d98f80d289ca64194f3b6843c
Jeremy Lewi <jeremy+github@lewi.us>
2019-04-24 06:12:03 -0700

    Add fire, gitpython and hub to test image and fix namespace packaging (#365)
    * Add fire, gitpython and hub to test image, fix namespace packaging.
    
    * fire is a python library that makes it easy to define entrypoints
    * gitpython is a python library for git; this will be used for gitops and CI/CD
    * hub is a CLI for github that will be used for gitops
    
    * Create a script to checkout repos. This is intended to be used in
      situations where we want to check out the latest code and then run some
      script.
    
    * Fix a bug in namespace package __init__.py has a typo.
    
    * Parameterize the makefile for the worker image so that we can build the worker image for project kubeflow-releasing using GCB.
    
    * Add a script for configuring permissions on ssh keys.
      * This can be used in an init container to setup ssh keyes;
        see
        https://github.com/jlewi/kubeflow-dev/blob/master/notes/git_on_kubeflow.md
    
    * exclude release-infra from lint

---
+++ images/checkout_repos.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+#
+# This script is used to checkout repositories.
+# This is typically used to bootstrap various scripts
+# by first checking out the code in an init container.
+#
+# For a pull request do
+# {REPO_ORG}/{REPO_NAME}@{SHA}:{PULL_NUMBER}
+#
+# You can use HEAD as the sha to get the latest for a pull request.
+#
+# You can use "--links" to create symbolic links to the newly created directories.
+# This allows you to check out forks of the Kubeflow repos but lay them out as if you
+# checked out the original repos so that you can use scripts that depend on that layout.
+set -xe
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+parseArgs() {
+  # Parse all command line options
+  while [[ $# -gt 0 ]]; do
+    # Parameters should be of the form
+    # --{name}=${value}
+    echo parsing "$1"
+    if [[ $1 =~ ^--(.*)=(.*)$ ]]; then
+      name=${BASH_REMATCH[1]}
+      value=${BASH_REMATCH[2]}
+
+      eval ${name}="${value}"
+    elif [[ $1 =~ ^--(.*)$ ]]; then
+    name=${BASH_REMATCH[1]}
+    value=true
+    eval ${name}="${value}"
+    else
+      echo "Argument $1 did not match the pattern --{name}={value} or --{name}"
+    fi
+    shift
+  done
+}
+
+usage() {
+  echo "Usage: checkout_repos --repos=<{REPO_ORG}/{REPO_NAME}@{SHA}:{PULL_NUMBER},{REPO_ORG}/{REPO_NAME}@HEAD:{PULL_NUMBER}> --src_dir=<Where to check them out> --links=<src1>=<dest1>,<src2>=<dest2>"
+}
+
+main() {
+
+  cd "${DIR}"
+
+  # List of required parameters
+  names=(repos src_dir)
+
+  missingParam=false
+  for i in ${names[@]}; do
+    if [ -z ${!i} ]; then
+      echo "--${i} not set"
+      missingParam=true
+    fi
+  done
+
+  if ${missingParam}; then
+    usage
+    exit 1
+  fi
+
+  # Check out any extra repos.
+  IFS=',' read -ra SPLIT_REPOS <<< "${repos}"
+  echo SPLIT_REPOS=${SPLIT_REPOS}
+  for r in "${SPLIT_REPOS[@]}"; do
+    echo "Processing ${r}" 
+    ORG_NAME="$(cut -d'@' -f1 <<< "$r")"
+    EXTRA_ORG="$(cut -d'/' -f1 <<< "$ORG_NAME")"
+    EXTRA_NAME="$(cut -d'/' -f2<<< "$ORG_NAME")"
+    SHA_AND_PR="$(cut -d'@' -f2 <<< "$r")"  
+    SHA="$(cut -d':' -f1 <<< "$SHA_AND_PR")"  
+    EXTRA_PR="$(cut -d':' -s -f2 <<< "$SHA_AND_PR")"
+    URL=https://github.com/${EXTRA_ORG}/${EXTRA_NAME}.git
+    TARGET=${src_dir}/${EXTRA_ORG}/${EXTRA_NAME}
+
+    mkdir -p ${src_dir}/${EXTRA_ORG}
+
+    if [ ! -d ${TARGET} ]; then
+      git clone  ${URL} ${TARGET}      
+    else 
+      # init containers might get restarted so its possible we already checked out the repo
+      echo ${TARGET} already exists
+    fi
+
+    cd ${TARGET}
+
+    if [ ! -z ${EXTRA_PR} ]; then
+      git fetch origin  pull/${EXTRA_PR}/head:pr
+      git checkout pr
+
+      if [ "$SHA" -ne "HEAD" ]; then
+        git checkout ${SHA}
+      fi      
+    else    
+      git checkout ${SHA}
+    fi
+    echo ${TARGET} is at `git describe --tags --always --dirty`
+  done  
+
+  IFS=',' read -ra LINKS <<< "${links}"
+  echo LINKS=${LINKS}
+  for r in "${LINKS[@]}"; do
+    link_src="$(cut -d'=' -f1 <<< "$r")"
+    link_dest="$(cut -d'=' -f2<<< "$r")"
+    # Ensure parent directory exists
+    mkdir -p $(dirname ${src_dir}/${link_dest})
+    ln -sf ${src_dir}/${link_src} ${src_dir}/${link_dest}
+  done
+}
+
+parseArgs $*
+main
