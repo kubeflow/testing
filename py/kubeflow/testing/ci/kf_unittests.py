044639652be3087a931e8dfc37f274bf9976374a
Scott Lee <scottlee@fastmail.com>
2019-08-02 13:37:22 -0700

    Support Python functions in workflows

---
+++ py/kubeflow/testing/ci/kf_unittests.py
@@ -0,0 +1,14 @@
+"""Test Argo workflow"""
+
+import requests
+import yaml
+
+def create_workflow():
+  """ Loads Argo example YAML and returns dictionary object """
+  # TODO: define workflow to run unittests
+  argo_hello_world = requests.get("https://raw.githubusercontent.com/argoproj/argo/master/examples/hello-world.yaml")
+  yaml_result = yaml.safe_load(argo_hello_world.text)
+  return yaml_result
+
+if __name__ == "__main__":
+  create_workflow()
