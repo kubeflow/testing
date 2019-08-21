b46ef52540d522f6e3331eaefd18edfa0f73fe51
Scott Lee <scottlee@fastmail.com>
2019-08-20 22:19:01 -0700

    Remove unused import

--- py/kubeflow/testing/run_e2e_workflow.py
+++ py/kubeflow/testing/run_e2e_workflow.py
@@ -60,7 +60,6 @@ from kubeflow.testing import argo_client
 from kubeflow.testing import ks_util
 from kubeflow.testing import prow_artifacts
 from kubeflow.testing import util
-from kubeflow.testing import ci
 
 # The namespace to launch the Argo workflow in.
 def get_namespace(args):
