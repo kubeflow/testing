7d571f16ece4803946211f24e141dc6a6676d44e
Scott Lee <scottlee@fastmail.com>
2019-08-20 21:58:22 -0700

    Fix pylint issues

--- py/kubeflow/testing/ci/kf_unittests.py
+++ py/kubeflow/testing/ci/kf_unittests.py
@@ -3,7 +3,7 @@
 import requests
 import yaml
 
-def create_workflow(**kwargs):
+def create_workflow():
   """ Loads Argo example YAML and returns dictionary object """
   # TODO: define workflow to run unittests
   argo_hello_world = requests.get("https://raw.githubusercontent.com/argoproj/"
