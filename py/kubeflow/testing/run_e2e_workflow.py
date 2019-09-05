44346201d31dcff7e4bb573ef556ad25114fb178
Scott Lee <scottlee@fastmail.com>
2019-09-04 22:07:49 -0700

    Fix include_dirs

--- py/kubeflow/testing/run_e2e_workflow.py
+++ py/kubeflow/testing/run_e2e_workflow.py
@@ -220,7 +220,7 @@ def run(args, file_handler): # pylint: disable=too-many-statements,too-many-bran
     # If we are scoping this workflow to specific directories, check if any files
     # modified match the specified regex patterns.
     dir_modified = False
-    if w.include_dirs:
+    if hasattr(w, "include_dirs") and w.include_dirs:
       for f in changed_files:
         for d in w.include_dirs:
           if fnmatch.fnmatch(f, d):
@@ -233,7 +233,7 @@ def run(args, file_handler): # pylint: disable=too-many-statements,too-many-bran
 
     # Only consider modified files when the job is pre or post submit, and if
     # the include_dirs stanza is defined.
-    if job_type != "periodic" and w.include_dirs and not dir_modified:
+    if job_type != "periodic" and hasattr(w, "include_dirs") and w.include_dirs and not dir_modified:
       logging.info("Skipping workflow %s because no code modified in %s.",
                    w.name, w.include_dirs)
       continue
