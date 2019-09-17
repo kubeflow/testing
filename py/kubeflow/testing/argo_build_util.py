39bf512a67fabce82fdff64f8f3d692641564ab3
Jeremy Lewi <jeremy@lewi.us>
2019-09-17 13:42:28 -0700

    kf_unittests should create a suitable argo workflow for the unittests
    * Create a simple CLI tool e2e_tool.py to support working with py_funcs
      for E2E tests
    
    * run_e2e_workflow.py should pass the name and namespace to the py_func.
    
    * Add to util.load_kube_config an option to skip printing out the kube
      config.
    
    * Remove ksonnet E2E tests.
    
    Related to kubeflow/kubeflow#3035 migrate e2e tests off ksonnet

---
+++ py/kubeflow/testing/argo_build_util.py
@@ -0,0 +1,96 @@
+"""Utilities for building argo workflows in python."""
+
+import os
+import yaml
+
+def get_prow_dict():
+  # see https://github.com/kubernetes/test-infra/blob/70015225876afea36de3ce98f36fe1592e8c2e53/prow/jobs.md  # pylint: disable=line-too-long
+  prow_vars = ["JOB_NAME", "JOB_TYPE", "JOB_SPEC", "BUILD_ID", "PROW_JOB_ID",
+               "REPO_OWNER", "REPO_NAME", "PULL_BASE_REF", "PULL_REFS",
+               "PULL_NUMBER", "PULL_PULL_SHA"]
+
+  d = {}
+  for v in prow_vars:
+    if not os.getenv(v):
+      continue
+
+    d[v] = os.getenv(v)
+
+  return d
+
+def get_prow_labels():
+  """Return a dictionary of prow labels suitable for use as labels"""
+  # see https://github.com/kubernetes/test-infra/blob/70015225876afea36de3ce98f36fe1592e8c2e53/prow/jobs.md # pylint: disable=line-too-long
+  prow_vars = ["JOB_NAME", "JOB_TYPE", "BUILD_ID", "PROW_JOB_ID",
+               "REPO_OWNER", "REPO_NAME", "PULL_NUMBER"]
+
+  d = {}
+  for v in prow_vars:
+    if not os.getenv(v):
+      continue
+
+    d[v] = os.getenv(v)
+
+  return d
+
+def add_prow_env(spec):
+  """Copy any prow environment variables to the step.
+
+  Args:
+    spec: Argo template spec
+
+  Returns:
+    spec: Updated spec
+  """
+
+  if not spec.get("container").get("env"):
+    spec["container"]["env"] = []
+
+  prow_dict = get_prow_dict()
+  for k, v in prow_dict.items():
+    spec["container"]["env"].append({"name": k,
+                                     "value": v})
+
+  for k, v in get_prow_labels().items():
+    spec["metadata"]["labels"][k] = v
+  return spec
+
+def deep_copy(d):
+  """Perform a deep copy of the supplied object"""
+  s = yaml.dump(d)
+  return yaml.load(s)
+
+def add_task_to_dag(workflow, dag_name, task, dependencies):
+  """Add a task to the specified workflow.
+
+  Args:
+    workflow: The workflow spec
+    dag_name: The name of the dag to add the step to
+    task: The task template
+    dependencies: A list of dependencies
+  """
+
+  dag = None
+  for t in workflow["spec"]["templates"]:
+    if "dag" not in t:
+      continue
+    if t["name"] == dag_name:
+      dag = t
+
+  if not dag:
+    raise ValueError("No dag named {0} found".format(dag_name))
+
+  if not dag["dag"].get("tasks"):
+    dag["dag"]["tasks"] = []
+
+  new_task = {
+    "name": task["name"],
+    "template": task["name"],
+  }
+
+  if dependencies:
+    new_task["dependencies"] = dependencies
+
+  dag["dag"]["tasks"].append(new_task)
+
+  workflow["spec"]["templates"].append(task)
--- py/kubeflow/testing/ci/kf_unittests.py
+++ py/kubeflow/testing/ci/kf_unittests.py
@@ -1,23 +1,258 @@
-"""
-A smoke test to ensure run_e2e_workflow.py can properly trigger workflows using
-a py_func.
+""""Define the E2E workflows used to run unittests."""
 
-This module defines a simple py_func which will return the Argo workflow
-resulting from loading the specified YAML file.
+from kubeflow.testing import argo_build_util
+import os
 
-The purpose of this is to allow the presubmits/postsubmits to verify that
-run_e2e_workflow.py is actually triggering workflows using py_func.
-"""
+# The name of the NFS volume claim to use for test files.
+NFS_VOLUME_CLAIM = "nfs-external"
+# The name to use for the volume to use to contain test data
+DATA_VOLUME = "kubeflow-test-volume"
 
-import requests
-import yaml
+EXIT_DAG_NAME = "exit-handler"
 
-def create_workflow(**kwargs):
-  """ Loads Argo example YAML and returns dictionary object """
-  # TODO: define workflow to run unittests
-  argo_hello_world = requests.get(''.join([v for k, v in kwargs.items()]))
-  yaml_result = yaml.safe_load(argo_hello_world.text)
-  return yaml_result
+WORKFLOW_TEMPLATE = {
+  "apiVersion": "argoproj.io/v1alpha1",
+  "kind": "Workflow",
+  "metadata": {
+  },
+  "spec": {
+    # Have argo garbage collect old workflows otherwise we overload the API
+    # server.
+    "ttlSecondsAfterFinished": 7 * 24 * 60 * 60,
+    "volumes": [
+      {
+        "name": "gcp-credentials",
+        "secret": {
+          "secretName": "kubeflow-testing-credentials",
+        },
+      },
+      {
+        "name": DATA_VOLUME,
+        "persistentVolumeClaim": {
+          "claimName": NFS_VOLUME_CLAIM,
+        },
+      },
+    ],
+    "onExit": EXIT_DAG_NAME,
+    "templates": [],
+  },  # spec
+} # workflow
 
-if __name__ == "__main__":
-  create_workflow()
+TEMPLATE_LABEL = "kf_unittests"
+
+DEFAULT_TEMPLATE = {'activeDeadlineSeconds': 3000,
+ 'container': {'command': [],
+  'env': [
+    {"name": "GOOGLE_APPLICATION_CREDENTIALS",
+     "value": "/secret/gcp-credentials/key.json"}
+   ],
+  'image': 'gcr.io/kubeflow-ci/test-worker:latest',
+  'imagePullPolicy': 'Always',
+  'name': '',
+  'resources': {'limits': {'cpu': '4', 'memory': '4Gi'},
+   'requests': {'cpu': '1', 'memory': '1536Mi'}},
+  'volumeMounts': [{'mountPath': '/mnt/test-data-volume',
+    'name': 'kubeflow-test-volume'},
+   {'mountPath': '/secret/gcp-credentials', 'name': 'gcp-credentials'}]},
+ 'metadata': {'labels': {
+   'workflow_template': TEMPLATE_LABEL}},
+ 'outputs': {}}
+
+def create_workflow(name=None, namespace=None, bucket="kubeflow-ci_temp"): # pylint: disable=too-many-statements
+  """Create workflow returns an Argo workflow to test kfctl upgrades.
+
+  Args:
+    name: Name to give to the workflow. This can also be used to name things
+     associated with the workflow.
+  """
+  workflow = WORKFLOW_TEMPLATE
+  workflow["metadata"]["name"] = name
+  workflow["metadata"]["namespace"] = namespace
+
+  workflow["metadata"]["labels"] = {
+    "workflow": name,
+    "workflow_template": TEMPLATE_LABEL,
+  }
+
+  e2e_dag_name = "e2e"
+
+  workflow["spec"]["entrypoint"] = e2e_dag_name
+
+  # Define the E2E dag
+  workflow["spec"]["templates"].extend([
+    {
+      "dag": {
+        "tasks": [],
+      },
+      "name": e2e_dag_name,
+    },
+    {
+      "dag": {
+        "tasks": [],
+      },
+      "name": EXIT_DAG_NAME,
+    }
+  ])
+
+  prow_dict = argo_build_util.get_prow_labels()
+
+  for k, v in prow_dict.items():
+    workflow["metadata"]["labels"][k] = v
+
+  #****************************************************************************
+  # Define directory locations
+  #****************************************************************************
+  # mount_path is the directory where the volume to store the test data
+  # should be mounted.
+  mount_path = "/mnt/" + "test-data-volume"
+  # test_dir is the root directory for all data for a particular test run.
+  test_dir = mount_path + "/" + name
+  # output_dir is the directory to sync to GCS to contain the output for this
+  # job.
+  output_dir = test_dir + "/output"
+  artifacts_dir = output_dir + "/artifacts"
+
+  # source directory where all repos should be checked out
+  src_root_dir = test_dir + "/src"
+  # The directory containing the kubeflow/kubeflow repo
+  src_dir = src_root_dir + "/kubeflow/kubeflow"
+
+  # Top level directories for python code
+  kubeflow_py = src_dir
+
+  # The directory within the kubeflow_testing submodule containing
+  # py scripts to use.
+  kubeflow_testing_py = src_root_dir + "/kubeflow/testing/py"
+
+  go_path = test_dir
+
+  # Define common environment variables to be added to all steps
+  common_env = [
+    {'name': 'PYTHONPATH',
+     'value': ":".join([kubeflow_py, kubeflow_testing_py])},
+    {'name': 'GOPATH',
+      'value': go_path},
+    {'name': 'KUBECONFIG',
+     'value': os.path.join(test_dir, 'kfctl_test/.kube/kubeconfig')},
+  ]
+
+  DEFAULT_TEMPLATE["container"]["env"].extend(common_env)
+
+  # create the checkout step
+  job_type = os.getenv("JOB_TYPE", "").lower()
+  if  job_type == "presubmit":
+    repos = "kubeflow/testing@{0}:{1}".format(
+      os.getenv("PULL_PULL_SHA", "HEAD"), os.getenv("PULL_NUMBER"))
+  elif job_type == "postsubmit":
+    repos = "kubeflow/testing@{0}".format(os.getenv("PULL_BASE_SHA"))
+  else:
+    branch = os.getenv("BRANCH_NAME", "HEAD")
+    repos = "kubeflow/testing@{0}".format(branch)
+
+  #**************************************************************************
+  # Checkout
+  checkout = argo_build_util.deep_copy(DEFAULT_TEMPLATE)
+  checkout = argo_build_util.add_prow_env(checkout)
+
+  checkout["name"] = "checkout"
+  checkout["container"]["command"] = ["/usr/local/bin/checkout_repos.sh",
+                                      "--repos=" + repos,
+                                      "--src_dir="+src_root_dir]
+  checkout["metadata"]["labels"] = {
+    'step_name': 'checkout',
+    "workflow": name,
+  }
+  checkout["metadata"]["labels"].update(prow_dict)
+
+  argo_build_util.add_task_to_dag(workflow, e2e_dag_name, checkout, [])
+
+  #**************************************************************************
+  # Run python unittests
+  py_tests = argo_build_util.deep_copy(DEFAULT_TEMPLATE)
+  py_tests = argo_build_util.add_prow_env(py_tests)
+
+  py_tests["name"] = "py-test"
+  py_tests["container"]["command"] = ["python",
+                                      "-m",
+                                      "kubeflow.testing.test_py_checks",
+                                      "--artifacts_dir=" + artifacts_dir,
+                                      # TODO(jlewi): Should we be searching
+                                      # the entire py/kubeflo/testing tree?
+                                      "--src_dir=" + kubeflow_testing_py + "kubeflow/tests"]
+
+  py_tests["metadata"]["labels"] = {
+    'step_name': 'py-test',
+    "workflow": name,
+  }
+  py_tests["metadata"]["labels"].update(prow_dict)
+
+  argo_build_util.add_task_to_dag(workflow, e2e_dag_name, py_tests, [checkout["name"]])
+
+
+  #*****************************************************************************
+  # py lint
+  #****************************************************************************
+  py_lint = argo_build_util.deep_copy(DEFAULT_TEMPLATE)
+  py_lint = argo_build_util.add_prow_env(py_lint)
+
+  py_lint["name"] = "py-lint"
+  py_lint["container"]["command"] = ["python",
+                                     "-m",
+                                     "kubeflow.testing.test_py_lint",
+                                     "--artifacts_dir=" + artifacts_dir,
+                                     "--src_dir=" + kubeflow_testing_py,
+                                     ]
+  py_lint["metadata"]["labels"] = {
+    'step_name': py_lint["name"],
+    "workflow": name,
+  }
+  py_lint["metadata"]["labels"].update(prow_dict)
+
+  argo_build_util.add_task_to_dag(workflow, e2e_dag_name, py_lint, [checkout["name"]])
+
+  #*****************************************************************************
+  # create_pr_symlink
+  #****************************************************************************
+  # TODO(jlewi): run_e2e_workflow.py should probably create the PR symlink
+  symlink = argo_build_util.deep_copy(DEFAULT_TEMPLATE)
+  symlink = argo_build_util.add_prow_env(symlink)
+
+  symlink["name"] = "create-pr-symlink"
+  symlink["container"]["command"] = ["python",
+                                     "-m",
+                                     "kubeflow.testing.prow_artifacts",
+                                     "--artifacts_dir=" + output_dir,
+                                     "create_pr_symlink",
+                                     "--bucket=" + bucket,
+                                     ]
+  symlink["metadata"]["labels"] = {
+    'step_name': symlink["name"],
+    "workflow": name,
+  }
+  symlink["metadata"]["labels"].update(prow_dict)
+
+  argo_build_util.add_task_to_dag(workflow, e2e_dag_name, symlink, [checkout["name"]])
+
+  #*****************************************************************************
+  # Exit handler workflow
+  #*****************************************************************************
+  copy_artifacts = argo_build_util.deep_copy(DEFAULT_TEMPLATE)
+  copy_artifacts = argo_build_util.add_prow_env(copy_artifacts)
+
+  copy_artifacts["name"] = "copy-artifacts"
+  copy_artifacts["container"]["command"] = ["python",
+                                            "-m",
+                                            "kubeflow.testing.prow_artifacts",
+                                            "--artifacts_dir=" + output_dir,
+                                            "copy_artifacts",
+                                            "--bucket=" + bucket,
+                                            "--suffix=fakesuffix",]
+  copy_artifacts["metadata"]["labels"] = {
+    'step_name': copy_artifacts["name"],
+    "workflow": name,
+  }
+  copy_artifacts["metadata"]["labels"].update(prow_dict)
+
+  argo_build_util.add_task_to_dag(workflow, EXIT_DAG_NAME, copy_artifacts, [])
+
+  return workflow
