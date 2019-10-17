16ea186441a45d8e74457317a177d232eea75a71
Jeremy Lewi <jeremy+github@lewi.us>
2019-10-17 15:59:59 -0700

    Support subdirectories for junit files and grouping in test grid by name (#490)
    * We want to support putting junit files in subdirectories e.g.
      junit/junit_workflow1/junit_test1.xml
      junit/junit_workflow2/junit_test1.xml
    
      * prow_artifacts.py needs to fetch the junit files from GCS in order to
        determine if there were any test failures to properly set the prow job
        status
    
      * The code however was making an assumption that all results were in
        the artifacts dir and not subdirectories
    
    * run_e2e_workflow.py should set a keyword argument test_target_name
      to the name of the workflow (as set in prow_config.yaml) for each of
      the python functions creating argo workflows.
    
      * We want to use the junit attribute classname to group tests coming
        from the same workflow. To do that we need to know the name of the workflow.
    
      * We also use this as a prefix for the test name to support grouping
        hierarchically
    
    * Related to kubeflow/testing#489
    
    * Update kfunittests to Use a junit subdirectory to make sure subdirectories are properly handled.
    
    * Update docs
    
    * update toc.
    
    * Convert test_py_lint to use pytest. Do this because we want to be able to test the use of junit features.
    
    * Add a separate mkdir step.

--- py/kubeflow/testing/run_e2e_workflow.py
+++ py/kubeflow/testing/run_e2e_workflow.py
@@ -69,6 +69,13 @@ import subprocess
 import sys
 import yaml
 
+# The name of the command line argument for workflows for the var
+# to contain the test target name.
+# The goal is to be able to use target name grouping in test grid
+# to group related tests
+# https://github.com/kubernetes/test-infra/tree/master/testgrid#grouping-tests
+TEST_TARGET_ARG_NAME = "test_target_name"
+
 # The namespace to launch the Argo workflow in.
 def get_namespace(args):
   if args.namespace:
@@ -328,6 +335,12 @@ def run(args, file_handler): # pylint: disable=too-many-statements,too-many-bran
     else:
       w.kwargs["name"] = workflow_name
       w.kwargs["namespace"] = get_namespace(args)
+
+      if TEST_TARGET_ARG_NAME not in w.kwargs:
+        w.kwargs[TEST_TARGET_ARG_NAME] = w.name
+        logging.info("Workflow %s doesn't set arg %s; defaulting to %s",
+                     w.name, TEST_TARGET_ARG_NAME,
+                     w.kwargs[TEST_TARGET_ARG_NAME])
 
       # TODO(https://github.com/kubeflow/testing/issues/467): We shell out
       # to e2e_tool in order to dumpy the Argo workflow to a file which then
