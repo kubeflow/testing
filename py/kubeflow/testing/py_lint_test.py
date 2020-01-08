cde229fb509c1a245d38536e5a5c5587831173f3
Jeremy Lewi <jlewi@google.com>
2020-01-07 22:16:59 -0800

    Skip lint due to #560

--- py/kubeflow/testing/py_lint_test.py
+++ py/kubeflow/testing/py_lint_test.py
@@ -39,6 +39,10 @@ def test_lint(record_xml_attribute, src_dir, rcfile): # pylint: disable=redefine
     "dashboard/frontend/node_modules",
     "kubeflow_testing",
     "dev-kubeflow-org/ks-app/vendor",
+    # TODO(https://github.com/kubeflow/testing/issues/560) stop skipping
+    # py/kubeflow/testing/cd once we update python & pylint so f style
+    # strings don't generate lint errors.
+    "py/kubeflow/testing/cd"
     "release-infra",
   ]
   full_dir_excludes = [
