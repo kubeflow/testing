737ddff5eec62e389cd957a7dffe870fb7a77f91
Gabriel Wen <gabrielwen@google.com>
2020-04-02 15:17:31 -0700

    update

--- py/kubeflow/testing/tekton_client.py
+++ py/kubeflow/testing/tekton_client.py
@@ -110,6 +110,10 @@ def get_namespaced_custom_object_with_retries(namespace, name):
   result = crd_api.get_namespaced_custom_object(
     GROUP, VERSION, namespace, PLURAL, name)
   log_status(result)
+  condition = workflow["status"]["conditions"][0]["reason"]
+  if not condition in ["Failed", "Succeeded"]:
+    raise ValueError("Waiting for %s/%s to finish", namespace, name)
+
   return result
 
 def load_tekton_run(workflow_name, params, test_target_name, tekton_run,
