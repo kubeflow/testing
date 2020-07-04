333963df6c15b17b37c5c05a959e9884a433b568
Jeremy Lewi <jlewi@google.com>
2020-07-04 11:11:21 -0700

    prctl can use a GitHub App to create PRs.
    * Using GitHub apps as opposed to personal access tokens for automation
      is preferred.
    
      * GitHub apps are owned by the org and multiple individuals can be
        granted permission to administer them
    
      * This is much better than creating a shared github account
    
    * Instead of shelling out to deprecated "hub CLI" for go; we are using
      the new GitHub CLI (github.com/cli/cli) as a library.
    
    * Remove hub from the docker image because we no longer use it.

--- go/cmd/prctl/main.go
+++ go/cmd/prctl/main.go
@@ -1,25 +1,39 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/bradleyfalzon/ghinstallation"
+	"github.com/cli/cli/api"
+	"github.com/kubeflow/testing/go/pkg/ghrepo"
 	"github.com/onrik/logrus/filename"
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"os/exec"
 	"regexp"
+	"strconv"
+	"strings"
 )
 
 type cliOptions struct {
-	upstreamName string
-	repoDir      string
-	forkName     string
-	fork         string
-	branchName   string
-	messagePath  string
-	baseBranch   string
-	refSpec string
+	upstreamName  string
+	repoDir       string
+	repo          string
+	forkName      string
+	fork          string
+	branchName    string
+	messagePath   string
+	baseBranch    string
+	refSpec       string
 	jsonLogFormat bool
+	privateKey    string
+	installId     string
+	appId         string
+	forkRef       string
 }
 
 var (
@@ -74,7 +88,7 @@ var (
 				log.Fatalf("Failed to setup the app; error: %+v", err)
 				return
 			}
-			err := pr(options.repoDir, options.baseBranch, options.messagePath)
+			err := pr(options.repo, options.baseBranch, options.forkRef, options.messagePath)
 
 			if err != nil {
 				log.Fatalf("push failed; error: %+v", err)
@@ -133,12 +147,18 @@ func init() {
 	pushCmd.MarkFlagRequired("forkName")
 	pushCmd.MarkFlagRequired("messagePath")
 
-	prCmd.Flags().StringVarP(&options.repoDir, "repoDir", "", "", "Directory where the code is checked out")
-	prCmd.Flags().StringVarP(&options.baseBranch, "baseBranch", "", "kubeflow:master", "Name of the branch to use as the base")
+	prCmd.Flags().StringVarP(&options.repo, "repo", "", "", "The repo to create the PR in in the form OWNER/REPO")
+	prCmd.Flags().StringVarP(&options.baseBranch, "baseBranch", "", "master", "Name of the branch to merge your changes into")
+	prCmd.Flags().StringVarP(&options.forkRef, "forkRef", "", "", "Reference to the branch to create the PR from; typically ${user}:${branch}")
 	prCmd.Flags().StringVarP(&options.messagePath, "messagePath", "", "", "Path to a file containing the message to use for the commit")
+	prCmd.Flags().StringVarP(&options.appId, "appId", "", "", "App id for the GitHub app")
+	prCmd.Flags().StringVarP(&options.privateKey, "privateKey", "", "", "Path to the file containing the private key for the GitHub app")
 
 	prCmd.MarkFlagRequired("baseBranch")
+	prCmd.MarkFlagRequired("forkRef")
 	prCmd.MarkFlagRequired("messagePath")
+	prCmd.MarkFlagRequired("privateKey")
+	prCmd.MarkFlagRequired("appId")
 
 	// Add filename as one of the fields of the structured log message.
 	filenameHook := filename.NewHook()
@@ -304,7 +324,6 @@ func push(repoDir string, forkName string, refSpec string, messagePath string) e
 		return errors.Wrapf(err, "There was an error staging allchanges.")
 	}
 
-
 	e = &execHelper{
 		cmd:      exec.Command("git", "commit", "-a", "-F", messagePath),
 		errorRes: gitErrorRegexes,
@@ -336,36 +355,189 @@ func push(repoDir string, forkName string, refSpec string, messagePath string) e
 	return nil
 }
 
-// pr creates a pull request
-func pr(repoDir string, baseBranch, messagePath string) error {
-	if repoDir == "" {
-		repoDir, err := os.Getwd()
-		if err != nil {
-			return errors.WithStack(errors.Errorf("repoDir not provided and couldn't get current directory; Error: %v", err))
-		}
-		log.Infof("Using current directory; %v", repoDir)
-	}
+func getInstallId(appId int64, privateKey string, owner string, repo string) (int64, error) {
+	tr := http.DefaultTransport
 
-	// TODO(jlewi): We might want to use gh here. gh is the new official CLI
-	// https://github.com/cli/cli/blob/trunk/docs/gh-vs-hub.md. According to the FAQ
-	// https://github.com/cli/cli/blob/trunk/docs/gh-vs-hub.md#should-i-use-gh-or-hub
-	// hub might be better for scripting.
-	// both are go so we could potentially link them in.
-	e := &execHelper{
-		cmd: exec.Command("hub", "pull-request", "-f", "-b", baseBranch, "-F", messagePath),
-		errorRes: map[string][]string{
-			"exists": {".*already exists.*"},
-		},
-	}
+	appTr, err := ghinstallation.NewAppsTransportKeyFromFile(tr, appId, privateKey)
 
-	e.cmd.Dir = repoDir
-
-	_, err := e.Run()
+	client := &http.Client{Transport: appTr}
 
 	if err != nil {
-		return errors.Wrapf(err, "There was an error creating the pull request.")
+		return 0, errors.WithStack(errors.Wrapf(err, "There was a problem getting the GitHub installation id"))
 	}
 
+	// Get the installtion id
+	url := fmt.Sprintf("https://api.github.com/repos/%v/%v/installation", owner, repo)
+	resp, err := client.Get(url)
+
+	if err != nil {
+		return 0, errors.WithStack(errors.Wrapf(err, "There was a problem getting the GitHub installation id"))
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		// TODO(jlewi): Should we try to read the body and include that in the error message?
+		return 0, errors.WithStack(errors.Wrapf(err, "There was a problem getting the GitHub installation id; Get %v returned statusCode %v; Response:\n%+v", url, resp.StatusCode, resp))
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return 0, errors.WithStack(errors.Wrapf(err, "There was a problem reading the response body."))
+	}
+
+	type idResponse struct {
+		Id int64 `json:"id"`
+	}
+
+	r := &idResponse{}
+	err = json.Unmarshal(body, r)
+
+	if err != nil {
+		return 0, errors.WithStack(errors.Wrapf(err, "Could not unmarshal json:\n %v", body))
+	}
+	return r.Id, nil
+}
+
+// pr creates a pull request
+// repoDir the directory of your repository.
+// baseBranch the branch into which your code should be merged.
+//
+// TODO(jlewi): We should be able to determine forkRef automatically from the local information in the repo.
+// On the other hand would it be better to not
+func pr(repo string, baseBranch, forkRef, messagePath string) error {
+	if repo == "" {
+		return errors.WithStack(errors.Errorf("repo must be provided in the form OWNER/REPO"))
+	}
+
+	appId, err := strconv.ParseInt(options.appId, 10, 64)
+
+	if err != nil {
+		return errors.WithStack(errors.Errorf("Could not convert appId %v to integer; error: %v", options.appId, err))
+	}
+
+	baseRepo, err := ghrepo.FromFullName(repo)
+
+	if err != nil {
+		return errors.WithStack(errors.Wrapf(err, "There was a problem getting the base repo."))
+	}
+
+	log.Infof("Base repository: %v/%v", baseRepo.RepoOwner(), baseRepo.RepoName())
+
+	// Shared transport to reuse TCP connections.
+	tr := http.DefaultTransport
+
+	installId, err := getInstallId(appId, options.privateKey, baseRepo.RepoOwner(), baseRepo.RepoName())
+
+	if err != nil {
+		return errors.WithStack(errors.Wrapf(err, "There was a problem getting the GitHub installation id"))
+	}
+
+	if installId == 0 {
+		return errors.WithStack(errors.Errorf("Could not obtain a GitHub installId for AppId %v on repo %v/%v; Is the app installed?", appId, baseRepo.RepoOwner(), baseRepo.RepoName()))
+	}
+
+	log.Infof("Repo %v/%v install id: %v", baseRepo.RepoOwner(), baseRepo.RepoName(), installId)
+
+	if err != nil {
+		return errors.WithStack(errors.Wrapf(err, "Could not obtain a GitHub installId for AppId %v on repo %v/%v; Is the app installed?", appId, baseRepo.RepoOwner(), baseRepo.RepoName()))
+	}
+
+	// Wrap the shared transport for use with the app ID 1 authenticating with installation ID 99.
+	itr, err := ghinstallation.NewKeyFromFile(tr, appId, installId, options.privateKey)
+
+	if err != nil {
+		return errors.WithStack(errors.Wrapf(err, "Error creating githubinstallation"))
+	}
+
+	client := api.NewClient(api.ReplaceTripper(itr))
+
+	// TODO(jlewi): Fill in with actual values by readin
+	messageBytes, err := ioutil.ReadFile(messagePath)
+
+	if err != nil {
+		return errors.WithStack(errors.Wrapf(err, "There was an error reading file: %v", messagePath))
+	}
+
+	lines := strings.SplitN(string(messageBytes), "\n", 2)
+
+	title := ""
+	body := ""
+
+	if len(lines) >= 1 {
+		title = lines[0]
+	}
+
+	if len(lines) >= 2 {
+		body = lines[1]
+	}
+
+	// For more info see:
+	// https://developer.github.com/v4/input_object/createpullrequestinput/
+	//
+	// body ad title can't be blank.
+	params := map[string]interface{}{
+		"title": title,
+		"body":  body,
+		"draft": false,
+		// The name of the branch to merge changes into.
+		"baseRefName": baseBranch,
+		// The name of the reference to merge changes from; typically in the form $user:$branch
+		"headRefName": forkRef,
+	}
+
+	// Forkref will either be OWNER:BRANCH when a different repository is used as the fork.
+	// Or it will be just BRANCH when merging from a branch in the same repo as repo
+	pieces := strings.Split(forkRef, ":")
+
+	forkOwner := baseRepo.RepoOwner()
+	forkBranch := ""
+	if len(pieces) == 1 {
+		forkBranch = pieces[0]
+	} else {
+		forkOwner = pieces[0]
+		forkBranch = pieces[1]
+	}
+
+	// Query the GitHub API to get actual repository info.
+	baseRepository, err := api.GitHubRepo(client, baseRepo)
+
+	if err != nil {
+		return errors.WithStack(errors.Wrapf(err, "There was an error getting repository information."))
+	}
+	pr, err := api.CreatePullRequest(client, baseRepository, params)
+	if err != nil {
+		graphErr, ok := err.(*api.GraphQLErrorResponse)
+		if ok {
+			for _, gErr := range graphErr.Errors {
+				if isMatch, _ := regexp.MatchString("A pull request already exists.*", gErr.Message); isMatch {
+					// TODO(jlewi): Might be useful to find and print out the URL of the existing PR
+					log.Info(gErr.Message)
+
+					// If the fork is in a different repo then the head reference is OWNER:BRANCH
+					// If we are creating the PR from a different branch in the same repo as where we are creating
+					// the PR then we just use BRANCH as the ref
+					headBranchRef := forkRef
+
+					if forkOwner == baseRepo.RepoOwner() {
+						headBranchRef = forkBranch
+					}
+
+					existingPR, err := api.PullRequestForBranch(client, baseRepo, baseBranch, headBranchRef)
+					var notFound *api.NotFoundError
+					if err != nil && errors.As(err, &notFound) {
+						return fmt.Errorf("error checking for existing pull request: %w", err)
+					}
+
+					log.Infof("A pull request for branch %q into branch %q already exists:\n%s", forkRef, baseBranch, existingPR.URL)
+
+					return nil
+				}
+			}
+		}
+
+		return errors.WithStack(errors.Wrapf(err, "Failed to create pull request"))
+	}
+
+	log.Infof("Created PR: %+v", pr.URL)
 	return nil
 }
 
---
+++ go/pkg/ghrepo/repo.go
@@ -0,0 +1,73 @@
+// package ghrepo is a copy of https://github.com/cli/cli/tree/trunk/internal/ghrepo
+// https://github.com/cli/cli/issues/1327 is tracking exporting it so we don't have to make a copy
+
+package ghrepo
+
+import (
+	"fmt"
+	"net/url"
+	"strings"
+)
+
+// TODO these are sprinkled across command, context, config, and ghrepo
+const defaultHostname = "github.com"
+
+// Interface describes an object that represents a GitHub repository
+type Interface interface {
+	RepoName() string
+	RepoOwner() string
+}
+
+// New instantiates a GitHub repository from owner and name arguments
+func New(owner, repo string) Interface {
+	return &ghRepo{
+		owner: owner,
+		name:  repo,
+	}
+}
+
+// FullName serializes a GitHub repository into an "OWNER/REPO" string
+func FullName(r Interface) string {
+	return fmt.Sprintf("%s/%s", r.RepoOwner(), r.RepoName())
+}
+
+// FromFullName extracts the GitHub repository information from an "OWNER/REPO" string
+func FromFullName(nwo string) (Interface, error) {
+	var r ghRepo
+	parts := strings.SplitN(nwo, "/", 2)
+	if len(parts) != 2 || parts[0] == "" || parts[1] == "" {
+		return &r, fmt.Errorf("expected OWNER/REPO format, got %q", nwo)
+	}
+	r.owner, r.name = parts[0], parts[1]
+	return &r, nil
+}
+
+// FromURL extracts the GitHub repository information from a URL
+func FromURL(u *url.URL) (Interface, error) {
+	if !strings.EqualFold(u.Hostname(), defaultHostname) && !strings.EqualFold(u.Hostname(), "www."+defaultHostname) {
+		return nil, fmt.Errorf("unsupported hostname: %s", u.Hostname())
+	}
+	parts := strings.SplitN(strings.TrimPrefix(u.Path, "/"), "/", 3)
+	if len(parts) < 2 {
+		return nil, fmt.Errorf("invalid path: %s", u.Path)
+	}
+	return New(parts[0], strings.TrimSuffix(parts[1], ".git")), nil
+}
+
+// IsSame compares two GitHub repositories
+func IsSame(a, b Interface) bool {
+	return strings.EqualFold(a.RepoOwner(), b.RepoOwner()) &&
+		strings.EqualFold(a.RepoName(), b.RepoName())
+}
+
+type ghRepo struct {
+	owner string
+	name  string
+}
+
+func (r ghRepo) RepoOwner() string {
+	return r.owner
+}
+func (r ghRepo) RepoName() string {
+	return r.name
+}
